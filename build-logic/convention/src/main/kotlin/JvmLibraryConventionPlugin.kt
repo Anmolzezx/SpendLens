6fd9489cb33019f3461b54eaf625730a788359ad
Anmol Singh <anmolsingh7442@gmail.com>
2026-08-30 03:42:28 +0530

    build: add JUnit to JVM library convention plugin

--- build-logic/convention/src/main/kotlin/JvmLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/JvmLibraryConventionPlugin.kt
@@ -1,8 +1,12 @@
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.api.tasks.testing.Test
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
@@ -21,6 +25,18 @@ class JvmLibraryConventionPlugin : Plugin<Project> {
             tasks.withType<KotlinCompile>().configureEach {
                 compilerOptions {
                     jvmTarget.set(JvmTarget.JVM_17)
+                }
+            }
+
+            val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+            dependencies {
+                "testImplementation"(libs.findLibrary("junit").get())
+            }
+
+            tasks.withType<Test>().configureEach {
+                // Surface which tests ran in CI logs, not just a pass/fail count.
+                testLogging {
+                    events("passed", "skipped", "failed")
                 }
             }
         }
